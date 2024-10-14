2d26f646d3175805b1820386c9f30c63a17f2409
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-14 16:44:45 +0000

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,24 @@ Rails.application.routes.draw do
   resources :photos
   devise_for :users
   devise_for :usernames
+
+  root "photos#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "photos#index"
+
+  # User routes
+  get "/users/sign_up", controller: "users", action: "new"
+  post "/users/sign_up", controller: "users", action: "create"
+
+  get "/users/feed", controller: "users", action: "feed"
+
+  get "users/discover", controller: "users", action: "discover"
+
+  get "/users/profile", controller: "users", action: "show"
+
+  get "/users/edit", controller: "users", action: "edit"
+  patch "/users/edit", controller: "users", action: "update"
+  delete "/users/delete", controller: "users", action: "destroy"
+
+
 end
