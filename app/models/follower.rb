4051c7359a0c454a8f844e19f2e5382b7da63a96
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-15 14:53:25 +0000

    followers updated

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -13,7 +13,7 @@ class Follower < ApplicationRecord
 
   belongs_to :recepient, class_name: 'User' , foreign_key: 'recepient_id'
 
-  validates :sender_id, uniqueness: { scope: :recepient_id }
+  validates :sender, class_name: "User", foreign_key: "sender_id"
 
   after_create :send_notification
 
