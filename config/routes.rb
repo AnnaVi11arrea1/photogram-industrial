26c4b11ecb16a7b3e9df9b7c42c0acb79a6d836f
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-16 21:25:06 +0000

    added username route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+  devise_for :users
+
   resources :likes
   resources :comments
   resources :followers
   resources :photos
-  devise_for :users
-  devise_for :usernames
 
+  get "/:username" => "users#show", as: :user
 end
