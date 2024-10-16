2eedd18d4591943c610a2afb821dec3a0378dd93
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-16 17:49:49 +0000

    removed comments from controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,4 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :usernames
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
-
 end
