f2365f7247bd5f6badee7974bcf4e33dd1db8752
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-16 16:26:55 +0000

    Added navbar and CDN assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,40 @@
 Rails.application.routes.draw do
-  resources :likes
+   root "photos#index"
+
+  devise_for :users
+
   resources :comments
   resources :followers
+  resources :likes
   resources :photos
-  devise_for :users
-  devise_for :usernames
 
-  root "photos#index"
+
+
+
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
-  # User routes
-  get "/users/sign_up", controller: "users", action: "new"
-  post "/users/sign_up", controller: "users", action: "create"
+  # # User routes
+  # get "/users/sign_up", controller: "users", action: "new"
+  # post "/users/sign_up", controller: "users", action: "create"
 
-  get "/users/sign_out", controller: "users", action: "sign_out"
-  delete "/users/sign_out", controller: "users", action: "goodbye_session"
+  # get "/users/sign_out", controller: "users", action: "sign_out"
+  # delete "/users/sign_out", controller: "users", action: "goodbye_session"
 
-  get "/users/:username", controller: "users", action: "index"
-  get "/users/edit", controller: "users", action: "edit"
-  patch "/users/edit", controller: "users", action: "update"
-  delete "/users/delete", controller: "users", action: "destroy"
+  # get "/users/:username", controller: "users", action: "index"
+  # get "/users/edit", controller: "users", action: "edit"
+  # patch "/users/edit", controller: "users", action: "update"
+  # delete "/users/delete", controller: "users", action: "destroy"
 
-  get "/users/feed", controller: "users", action: "feed"
-  get "users/discover", controller: "users", action: "discover"
-  get "/users/profile", controller: "users", action: "show"
+  # get "/users/feed", controller: "users", action: "feed"
+  # get "users/discover", controller: "users", action: "discover"
+  # get "/users/profile", controller: "users", action: "show"
 
-  #photos 
-  get "photos/edit", controller: "photos", action: "edit"
-  post "photos/create", controller: "photos", action: "create"
-  get "photos/:id/comments", controller: "comments", action: "index"
-  patch "photos/edit", controller: "photos", action: "update"
-  delete "photos/delete", controller: "photos", action: "destroy"
+  # #photos 
+  # get "photos/edit", controller: "photos", action: "edit"
+  # post "photos/create", controller: "photos", action: "create"
+  # get "photos/:id/comments", controller: "comments", action: "index"
+  # patch "photos/edit", controller: "photos", action: "update"
+  # delete "photos/delete", controller: "photos", action: "destroy"
 end
