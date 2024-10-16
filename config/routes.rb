148216f970eda9cf65d42cd723592c3d67162bc2
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-16 17:45:52 +0000

    routes simplified and users controller removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
   resources :likes
   resources :comments
   resources :followers
@@ -6,30 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :usernames
 
-  root "photos#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
-  # User routes
-  get "/users/sign_up", controller: "users", action: "new"
-  post "/users/sign_up", controller: "users", action: "create"
 
-  get "/users/sign_out", controller: "users", action: "sign_out"
-  delete "/users/sign_out", controller: "users", action: "goodbye_session"
-
-  get "/users/:username", controller: "users", action: "index"
-  get "/users/edit", controller: "users", action: "edit"
-  patch "/users/edit", controller: "users", action: "update"
-  delete "/users/delete", controller: "users", action: "destroy"
-
-  get "/users/feed", controller: "users", action: "feed"
-  get "users/discover", controller: "users", action: "discover"
-  get "/users/profile", controller: "users", action: "show"
-
-  #photos 
-  get "photos/edit", controller: "photos", action: "edit"
-  post "photos/create", controller: "photos", action: "create"
-  get "photos/:id/comments", controller: "comments", action: "index"
-  patch "photos/edit", controller: "photos", action: "update"
-  delete "photos/delete", controller: "photos", action: "destroy"
 end
