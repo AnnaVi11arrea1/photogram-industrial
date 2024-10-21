7c18025707c56defffab90f0b2bc5ca87f26107e
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-21 21:07:18 +0000

    copying main routes into branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  root "photos#index"
 
+  devise_for :users
 
-
-  
+  resources :likes
   resources :comments
   resources :followers
   resources :likes
   resources :photos
 
-
   get "/:username" => "users#show", as: :user
-
 end
