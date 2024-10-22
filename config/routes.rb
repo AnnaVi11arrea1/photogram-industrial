a457f4644142b612e7260ddafd1cadb41503ac82
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-22 18:02:51 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,33 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  
-  
-  get "/users/:id" => "users#edit"
-  
-  get "/:username/liked" => "users#likes", as: :liked
-  
-  get "/:username/followers" => "users#followers", as: :followers
-  
-  get "/:username/following" => "users#following", as: :following
-  
-  get "/:username/feed" => "users#feed", as: :feed
-  
-  get "/:username/discover" => "users#discover", as: :discover
-  
-  get "/:username" => "users#show", as: :user
+  devise_for :users
+  devise_for :usernames
 
-  
+  root "photos#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
+  # User routes
+  get "/users/sign_up", controller: "users", action: "new"
+  post "/users/sign_up", controller: "users", action: "create"
+
+  get "/users/sign_out", controller: "users", action: "sign_out"
+  delete "/users/sign_out", controller: "users", action: "goodbye_session"
+
+  get "/users/:username", controller: "users", action: "index"
+  get "/users/edit", controller: "users", action: "edit"
+  patch "/users/edit", controller: "users", action: "update"
+  delete "/users/delete", controller: "users", action: "destroy"
+
+  get "/users/feed", controller: "users", action: "feed"
+  get "users/discover", controller: "users", action: "discover"
+  get "/users/profile", controller: "users", action: "show"
+
+  #photos 
+  get "photos/edit", controller: "photos", action: "edit"
+  post "photos/create", controller: "photos", action: "create"
+  get "photos/:id/comments", controller: "comments", action: "index"
+  patch "photos/edit", controller: "photos", action: "update"
+  delete "photos/delete", controller: "photos", action: "destroy"
 end
