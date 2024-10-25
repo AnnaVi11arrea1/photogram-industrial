2904037029130bd730baa6fa5ad74288e5e3e51e
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-25 15:24:56 +0000

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,50 +6,16 @@ Rails.application.routes.draw do
   resources :likes
   resources :comments
   resources :follow_requests
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ed7cb13 (changed followers to follow request, fixed routes)
-  resources :likes
   resources :photos
   
-<<<<<<< HEAD
-=======
-  
->>>>>>> 2cdf639 (fixing routes)
   get "/users/:id" => "users#edit"
   
   get "/:username/liked" => "users#likes", as: :liked
-  
-<<<<<<< HEAD
-<<<<<<< HEAD
-  get "/:username/followers" => "users#followers", as: :followers
-=======
-  get "/:username/followers" => "users#followers"
->>>>>>> 2cdf639 (fixing routes)
-=======
-  get "/:username/followers" => "users#followers", as: :followers
->>>>>>> ed7cb13 (changed followers to follow request, fixed routes)
-  
-  get "/:username/following" => "users#following", as: :following
-  
-  get "/:username/feed" => "users#feed", as: :feed
-  
-  get "/:username/discover" => "users#discover", as: :discover
-  
-  get "/:username" => "users#show", as: :user
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  resources :photos
->>>>>>> f197ce4 (fix routes)
-=======
->>>>>>> 2cdf639 (fixing routes)
 
-  get "/users/:id" => "users#edit"
-  
-  get "/:username/liked" => "users#likes", as: :liked
-  
+  get "/:username/followers" => "users#followers", as: :followers
+
+  get "/:username/followers" => "users#followers"
+
   get "/:username/followers" => "users#followers", as: :followers
   
   get "/:username/following" => "users#following", as: :following
@@ -59,6 +25,5 @@ Rails.application.routes.draw do
   get "/:username/discover" => "users#discover", as: :discover
   
   get "/:username" => "users#show", as: :user
-=======
->>>>>>> 70d12e0 (add photo working)
+
 end
